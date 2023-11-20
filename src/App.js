f92b5e86a496b5fc7d6b7b65f997915d52daa56d
kunaldec022002 <kunalingale72@gmail.com>
2023-11-20 11:13:17 +0530

    adding react routing in app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-
-import './App.css';
-
+import './App.css'
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom"
+import Header from './Components/Header/Header';
+import Home from './Pages/Home/Home';
+import MovieList from './Components/movielist/movieList';
+import Movie from './Pages/Home/movieDetail/movie';
 function App() {
+
   return (
     <div className="App">
-
-      <h1>Movie App</h1>
-      
+      <Router>
+        <Header />
+        <Routes>
+          <Route extact path="/" element={<Home/>} />
+          <Route path="movie/:id" element={<Movie/>} />
+          <Route path="movies/:type" element={<MovieList/>} />
+          <Route path="/*" element={<h2>Error page</h2>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default App
